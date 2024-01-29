b96b9a82e62728e4127baf903261e3647b184839
B0202 <157682651+B0202@users.noreply.github.com>
2024-01-29 11:42:37 +0530

    Update PIMStepDefinitions.cs

--- StepDefinitions/PIMStepDefinitions.cs
+++ StepDefinitions/PIMStepDefinitions.cs
@@ -93,8 +93,8 @@ namespace SpecFlowProject1.StepDefinitions
         public void ThenTimeModulePageShouldBeDisplayed()
         {
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//*[@class='oxd-topbar-header-title']")));
-            string Time_Act_title3 = driver.FindElement(By.XPath("//*[@class='oxd-topbar-header-title']")).Text;
-            Assert.AreEqual(Time_Exp_Title, Time_Act_title3);
+            string Time_Act_title4 = driver.FindElement(By.XPath("//*[@class='oxd-topbar-header-title']")).Text;
+            Assert.AreEqual(Time_Exp_Title, Time_Act_title4);
         }
 
 
